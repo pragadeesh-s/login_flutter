8e285530214b77cc7fbc1ec37b4f26425df38086
pragadeesh-s <pragadeeshsubash0@gmail.com>
2024-01-02 10:15:15 +0530

    .

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,121 +15,120 @@ class _LoginScreen extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Add Lead'),
-        centerTitle: false,
-      ),
-      body: SafeArea(
-        child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/Log in.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 45.0),
-              const Text(
-                'Caring is the new marketing',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 35.0,
-                  // fontFamily: 'Inter',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/Log in.png'),
+                fit: BoxFit.cover,
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
-                child: Text(
-                  'Physiological respiration involves the mechanisms that ensure that the compositio',
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 45.0),
+                const Text(
+                  'Caring is the new marketing',
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 14.0,
+                    fontSize: 35.0,
                     // fontFamily: 'Inter',
-                    fontWeight: FontWeight.normal,
+                    fontWeight: FontWeight.bold,
                     color: Colors.white,
                   ),
                 ),
-              ),
-              const SizedBox(height: 290.0),
-              // const CircleAvatar(
-              //   backgroundImage: AssetImage("assets/images/Rectangle_2188.png"),
-              //   radius: 80.0,
-              //   // child:
-              // ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 0.0),
-                child: TextField(
-                  controller: email,
-                  decoration: InputDecoration(
-                    contentPadding:
-                        const EdgeInsets.fromLTRB(40.0, 17.0, 40.0, 17.0),
-                    labelText: 'User ID',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40.0),
+                const Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 40.0),
+                  child: Text(
+                    'Physiological respiration involves the mechanisms that ensure that the compositio',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      // fontFamily: 'Inter',
+                      fontWeight: FontWeight.normal,
+                      color: Colors.white,
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 0.0),
-                child: TextField(
-                  //textAlign: TextAlign.center,
-                  controller: password,
-                  decoration: InputDecoration(
-                    contentPadding:
-                        const EdgeInsets.fromLTRB(40.0, 17.0, 40.0, 17.0),
-                    //floatingLabelAlignment: FloatingLabelAlignment.center,
-                    labelText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40.0),
+                const SizedBox(height: 290.0),
+                // const CircleAvatar(
+                //   backgroundImage: AssetImage("assets/images/Rectangle_2188.png"),
+                //   radius: 80.0,
+                //   // child:
+                // ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 0.0),
+                  child: TextField(
+                    controller: email,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.fromLTRB(40.0, 17.0, 40.0, 17.0),
+                      labelText: 'User ID',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
-                child: OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    backgroundColor: const Color.fromRGBO(134, 0, 213, 1.0),
-                    foregroundColor: Colors.white,
-                    padding:
-                        const EdgeInsets.fromLTRB(128.0, 15.0, 128.0, 15.0),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 0.0),
+                  child: TextField(
+                    //textAlign: TextAlign.center,
+                    controller: password,
+                    decoration: InputDecoration(
+                      contentPadding:
+                          const EdgeInsets.fromLTRB(40.0, 17.0, 40.0, 17.0),
+                      //floatingLabelAlignment: FloatingLabelAlignment.center,
+                      labelText: 'Password',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
+                    ),
                   ),
-                  onPressed: () async {
-                    var result =
-                        await ApiCall().apiCall(email.text, password.text);
-                    if (result?.message == "Token Generated Successfully") {
-                      Navigator.pushReplacementNamed(context, '/home');
-                    } else {
-                      showDialog(
-                        context: context,
-                        builder: (ctx) => AlertDialog(
-                          title: const Text(
-                            'Login Error',
-                          ),
-                          content: const Text(
-                            "Kindly check the email or password whether it is correct!",
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(ctx);
-                              },
-                              child: const Text('Okay'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0.0),
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: const Color.fromRGBO(134, 0, 213, 1.0),
+                      foregroundColor: Colors.white,
+                      padding:
+                          const EdgeInsets.fromLTRB(128.0, 15.0, 128.0, 15.0),
+                    ),
+                    onPressed: () async {
+                      var result =
+                          await ApiCall().apiCall(email.text, password.text);
+                      if (result?.message == "Token Generated Successfully") {
+                        Navigator.pushReplacementNamed(context, '/home');
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (ctx) => AlertDialog(
+                            title: const Text(
+                              'Login Error',
                             ),
-                          ],
-                        ),
-                      );
-                    }
-                  },
-                  child: const Text('LOGIN'),
+                            content: const Text(
+                              "Kindly check the email or password whether it is correct!",
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(ctx);
+                                },
+                                child: const Text('Okay'),
+                              ),
+                            ],
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text('LOGIN'),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
